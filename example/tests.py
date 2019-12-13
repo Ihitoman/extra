7e0878f5d8a2efb4b6d9af9e2f3127fc888a2a70
Ihitoman <163211@ids.upchiapas.edu.mx>
2019-12-13 12:04:27 -0600

    dfsdf

--- example/tests.py
+++ example/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from example.models import Cosa
 
+class CosaTest(TestCase):
+    def setUp(self):
+        Cosa.objects.Create(name = "prueba", actividad_id=1)
+
+    def test_post(self):
+        cosa1 = Cosa.objects.get(pk=4)
+        self.assertEqual(cosa1.name, 'prueba')
 # Create your tests here.
